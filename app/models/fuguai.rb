587994216813034308830cb78581cf32c93352c0
firstName lastName <emailAddress>
2013-02-05 11:19:22 +0900

    validatehチェックを追加

--- app/models/fuguai.rb
+++ app/models/fuguai.rb
@@ -1,3 +1,32 @@
 class Fuguai < ActiveRecord::Base
   attr_accessible :bhyo_no, :biko, :fuguai_no, :fuguaitenpu_kubun, :genin_kubun, :hako_ymd, :hakosya, :hasseikankyo, :hasseinaiyo, :judaido, :kaisekikekka, :kaito_ymd, :kaitosya, :kakuninsya, :kanryo_ymd, :kenmei, :kensyutukotei, :kinomei, :kotei_kubun, :saigensei, :sihen_no, :status_kubun, :syori_kubun, :systemmei, :taisyo_kubun, :taisyo_ymd, :taisyobutumei, :taisyoirai_ymd, :taisyosya, :taisyosyoti, :taisyotenpu_kubun, :taisyoyotei_ymd, :tyofuku_kubun, :tyofuku_no
+
+#入力チェック（ブランクは許しません！）
+  validates_presence_of :hakosya
+  validates_presence_of :hako_ymd
+  validates_presence_of :kenmei
+  validates_presence_of :systemmei
+  validates_presence_of :kotei_kubun
+  validates_presence_of :kinomei
+  validates_presence_of :saigensei
+  validates_presence_of :judaido
+  validates_presence_of :taisyoirai_ymd
+  validates_presence_of :hasseikankyo
+  validates_presence_of :fuguaitenpu_kubun
+  validates_presence_of :taisyoirai_ymd
+  validates_presence_of :hasseinaiyo
+  validates_presence_of :genin_kubun
+  validates_presence_of :kensyutukotei
+  validates_presence_of :taisyo_kubun
+  validates_presence_of :syori_kubun
+  validates_presence_of :taisyotenpu_kubun
+  validates_presence_of :kaito_ymd
+  validates_presence_of :kaitosya
+  validates_presence_of :taisyosyoti
+  validates_presence_of :taisyoyotei_ymd
+  validates_presence_of :taisyosya
+  validates_presence_of :taisyobutumei
+  validates_presence_of :kanryo_ymd
+  validates_presence_of :kakuninsya
+  validates_presence_of :status_kubun
 end
